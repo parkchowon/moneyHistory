46bb0ef2ef203212e565b0158a91cb675e71296d
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-05-22 19:34:33 +0900

    add : 초기세팅

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CreateHistory from "./components/CreateHistory";
+
+function App() {
+  return (
+    <>
+      <CreateHistory />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CreateHistory/CreateHistory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateHistory() {
+  return <div>CreateHistory</div>;
+}
+
+export default CreateHistory;
