7ff98c455d15ddd25bae949b13cc7a9145b26efd
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-05-30 18:03:10 +0900

    fix: router basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import router from "./routes/router";
 function App() {
   return (
     <Wrapper>
-      <RouterProvider router={router} />
+      <RouterProvider basename="/moneyHistory" router={router} />
     </Wrapper>
   );
 }
