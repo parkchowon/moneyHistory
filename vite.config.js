6a5d13c8f31f844ac34be04b5211cd210a3e6f8a
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-05-30 16:19:43 +0900

    add: dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/moneyHistory/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
